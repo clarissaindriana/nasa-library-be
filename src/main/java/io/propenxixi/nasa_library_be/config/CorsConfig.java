3f9473624fe5b794d7b4dc44c058a3053428768f
Clarissa Indriana <clarissaindriana@Clarissas-MacBook-Pro.local>
2026-02-07 15:14:44 +0700

    feat: cors config

---
+++ src/main/java/io/propenxixi/nasa_library_be/config/CorsConfig.java
@@ -0,0 +1,36 @@
+package io.propenxixi.nasa_library_be.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Value("${CORS_ALLOWED_ORIGINS:http://localhost:3000}")
+    private String allowedOrigins;
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                String[] origins = allowedOrigins.split(",");
+                
+
+                for (int i = 0; i < origins.length; i++) {
+                    origins[i] = origins[i].trim();
+                }
+
+                registry.addMapping("/**")
+                        .allowedOrigins(origins)
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true)
+                        .exposedHeaders("Authorization");
+            }
+        };
+    }
+}
